6caeb9ea7158f822f6f33697f0149e16d6e73bc1
johame72 <johnhmerkel@gmail.com>
2023-10-08 10:57:51 -0500

    Updated Figma Code 1st try

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="frame">
+      <div className="overlap">
+        <div className="div" />
+        <div className="rectangle-2" />
+        <div className="rectangle-3" />
+        <div className="rectangle-4" />
+        <div className="text-wrapper">ChatFormat</div>
+      </div>
+      <div className="overlap-group">
+        <div className="overlap-2">
+          <div className="overlap-group-2">
+            <div className="rectangle-5" />
+            <div className="rectangle-6" />
+            <div className="rectangle-7" />
+            <div className="rectangle-8" />
+            <div className="rectangle-9" />
+            <div className="rectangle-10" />
+            <div className="rectangle-11" />
+            <div className="rectangle-12" />
+          </div>
+          <div className="rectangle-13" />
+          <div className="rectangle-14" />
+        </div>
+        <div className="rectangle-15" />
+      </div>
+      <div className="rectangle-16" />
     </div>
   );
-}
-
-export default App;
+};
