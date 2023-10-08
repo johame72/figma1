9eb0138d822d34937d2e9aca5c327c7dc9137812
johame72 <johnhmerkel@gmail.com>
2023-10-08 16:34:58 -0500

    Updated App.js code to remove error

--- src/App.js
+++ src/App.js
@@ -27,35 +27,31 @@ const App = () => {
           <div className="y-axis">Y</div>
         </div>
       )}
-const App = () => {
-  return (
-    <div className="frame">
-      <div className="measurement-grid">
-        <div className="overlap">
-          <div className="div" />
-          <div className="rectangle-2" />
-          <div className="rectangle-3" />
-          <div className="rectangle-4" />
-          <div className="text-wrapper">ChatFormat</div>
-        </div>
-        <div className="overlap-group">
-          <div className="overlap-2">
-            <div className="overlap-group-2">
-              <div className="rectangle-5" />
-              <div className="rectangle-6" />
-              <div className="rectangle-7" />
-              <div className="rectangle-8" />
-              <div className="rectangle-9" />
-              <div className="rectangle-10" />
-              <div className="rectangle-11" />
-              <div className="rectangle-12" />
-            </div>
-            <div className="rectangle-13" />
-            <div className="rectangle-14" />
+
+      <div className="overlap">
+        <div className="div" />
+        <div className="rectangle-2" />
+        <div className="rectangle-3" />
+        <div className="rectangle-4" />
+        <div className="text-wrapper">ChatFormat</div>
+      </div>
+      <div className="overlap-group">
+        <div className="overlap-2">
+          <div className="overlap-group-2">
+            <div className="rectangle-5" />
+            <div className="rectangle-6" />
+            <div className="rectangle-7" />
+            <div className="rectangle-8" />
+            <div className="rectangle-9" />
+            <div className="rectangle-10" />
+            <div className="rectangle-11" />
+            <div className="rectangle-12" />
           </div>
-          <div className="rectangle-15" />
+          <div className="rectangle-13" />
+          <div className="rectangle-14" />
         </div>
-    </div>
+        <div className="rectangle-15" />
+      </div>
       <div className="rectangle-16" />
     </div>
   );
